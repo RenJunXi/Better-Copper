0bbc8d84cb24d1b9af8b47f9b1326bba4ad0f723
alan <769825994@qq.com>
2022-04-20 17:22:14 +0800

    Second Commit

--- src/main/java/com/renjunxi/mod/bettercopper/Bettercopper.java
+++ src/main/java/com/renjunxi/mod/bettercopper/Bettercopper.java
@@ -1,10 +1,22 @@
 package com.renjunxi.mod.bettercopper;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Bettercopper implements ModInitializer {
+    public static ToolItem COPPER_SWORD = new SwordItem(org.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, 1, 3, new Item.Settings().group(ItemGroup.TOOLS));
+    public static ToolItem COPPER_AXE = new org.renjunxi.mod.bettercopper.CopperAxe(org.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, 4, -3.2F, new Item.Settings().group(ItemGroup.TOOLS));
+    public static ToolItem COPPER_PICKAXE = new org.renjunxi.mod.bettercopper.CopperPickAxe(org.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, -4, -3.2F, new Item.Settings().group(ItemGroup.TOOLS));
     @Override
-    public void onInitialize() {
-
+    public void onInitialize()
+    {
+        Registry.register(Registry.ITEM, new Identifier("better_copper", "better_copper.copper_axe"), COPPER_AXE);
+        Registry.register(Registry.ITEM, new Identifier("better_copper", "better_copper.copper_sword"), COPPER_SWORD);
+        Registry.register(Registry.ITEM, new Identifier("better_copper", "better_copper.copper_pickaxe"), COPPER_PICKAXE);
     }
 }
