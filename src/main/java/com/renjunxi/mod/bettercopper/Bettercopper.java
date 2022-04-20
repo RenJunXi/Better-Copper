08f23388594850c645f0861717ede36f9842b597
alan <769825994@qq.com>
2022-04-20 17:15:23 +0800

    First Commit

---
+++ src/main/java/com/renjunxi/mod/bettercopper/Bettercopper.java
@@ -0,0 +1,10 @@
+package com.renjunxi.mod.bettercopper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bettercopper implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
